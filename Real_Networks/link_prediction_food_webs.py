b32d987f96952e6c7dc8cca5f256947b533b6c4d
Lucy Van Kleunen <lucy.vankleunen@kuleuven.be>
2025-12-13 17:08:42 +0100

    updating file paths, etc. for usability

--- Real_Networks/link_prediction_food_webs.py
+++ Real_Networks/link_prediction_food_webs.py
@@ -19,7 +19,7 @@ import pickle
 import link_prediction_helper
 
 # Note that the version of the data this is run on can be change by changing this variable
-Processed_Data_Version = 'Processed_Data_Disaggregated_Lifestage'
+Processed_Data_Version = 'Input_Data_Disaggregated_Lifestage'
 
 # Get saved information on food web attribute types and ids
 with open(os.path.join(Processed_Data_Version, 'fw_attr_types.pickle'), 'rb') as handle:
@@ -28,18 +28,18 @@ with open(os.path.join(Processed_Data_Version, 'fw_ids.pickle'), 'rb') as handle
     fw_ids = pickle.load(handle)
 
 # The following seeds correspond to those used for each iteration in the paper
-# Processed_Data_Disaggregated_Lifestage - seed=55 --> Results_Food_Webs_0
-# Processed_Data_Disaggregated_Lifestage_1 - seed=222 --> Results_Food_Webs_1
-# Processed_Data_Disaggregated_Lifestage_2 - seed=333 --> Results_Food_Webs_2
-# Processed_Data_Disaggregated_Lifestage_3 - seed=555 --> Results_Food_Webs_3
-# Processed_Data_Disaggregated_Lifestage_4 - seed=666 --> Results_Food_Webs_4
-# Processed_Data_Aggregated_Lifestage - seed=55 --> Results_Food_Webs_Aggregated
+# Input_Data_Disaggregated_Lifestage - seed=55 --> Results_Food_Webs_0
+# Input_Data_Disaggregated_Lifestage_1 - seed=222 --> Results_Food_Webs_1
+# Input_Data_Disaggregated_Lifestage_2 - seed=333 --> Results_Food_Webs_2
+# Input_Data_Disaggregated_Lifestage_3 - seed=555 --> Results_Food_Webs_3
+# Input_Data_Disaggregated_Lifestage_4 - seed=666 --> Results_Food_Webs_4
+# Input_Data_Aggregated_Lifestage - seed=55 --> Results_Food_Webs_Aggregated
 
 # Link prediction comparison arguments 
 details = False
 extra_links = False
 res_folder = 'Results_Food_Webs_0'
-seed = 55 # set a seed for reproducing the same results as those in the paper (on Processed_Data_Disaggregated_Lifestage)
+seed = 55 # set a seed for reproducing the same results as those in the paper (on Input_Data_Disaggregated_Lifestage)
 
 # short (in processed data) to long name (in results files) dictionary
 folder_shorter_names = {'Grand Caricaie Clmown1':'Grand Caricaie  marsh dominated by Cladietum marisci, mown  Clmown1',\
